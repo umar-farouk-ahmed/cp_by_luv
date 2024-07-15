763c6420c60027d7f0f1b7d1061c37f5aa60ad61
Umar Farouk Ahmed <umarahmed292@gmail.com>
2024-07-15 14:26:42 +0600

    1 practise done

--- 12_Pre-Computation_&_hashing/practise/Array.cpp
+++ 12_Pre-Computation_&_hashing/practise/Array.cpp
@@ -1,5 +1,26 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
+
+const int N = 1e5;
+int hsh[N];
+
 int main(){
-    
+    int n;
+    cout << "Enter the size of the array: "<<endl;
+    cin >> n;
+    int a[n];
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+        hsh[a[i]]++;
+    }
+    int t;
+    cout<<"Enter test cases : "<<endl;
+    cin >> t;
+    while(t>0){
+        int x;
+        cout << "Enter a number  to see frequency: "<<endl;
+        cin >> x;
+        cout <<"Ans: "<< hsh[x] <<endl;
+        t--;
+    }
 }
